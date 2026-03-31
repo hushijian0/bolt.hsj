8648805620d40775f78052943882da8f46d2e782
shijian.hu <shijian.hu@dreamsmart.com>
2026-03-31 10:56:26 +0800

    chore: update branding and fix pre-commit typecheck

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,6 +2,8 @@ import type { ServerBuild } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 export const onRequest: PagesFunction = async (context) => {
+  // `build/server` is generated at build time and doesn't exist during local type-checks.
+  // @ts-expect-error runtime-generated module
   const serverBuild = (await import('../build/server')) as unknown as ServerBuild;
 
   const handler = createPagesFunctionHandler({
